ce4699abc9048e715cfc67342eb9ef82741bab97
Imran Ahmed <imranahmed.ain@gmail.com>
2026-06-24 19:44:47 +0300

    InvalidApplicationException

---
+++ src/InvalidApplicationException.java
@@ -0,0 +1,7 @@
+public class InvalidApplicationException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public InvalidApplicationException(String message) {
+        super(message);
+    }
+}
